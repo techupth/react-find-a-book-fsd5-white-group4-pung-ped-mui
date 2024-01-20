d51a4001f92974aff9d106c329cd9bcdd681d4bf
bread <chalida.bread@gmail.com>
2024-01-20 16:40:36 +0700

    fix:edit code for add search condition for books

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
+import axios from "axios";
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
-}
+  const [findABook, setfindABook] = useState("");
+  const [resultFindABook, setResultFindABook] = useState([]);
 
+  useEffect(() => {
+    if (findABook) {
+      fetchABook(findABook);
+    }
+  }, [findABook]);
+
+  const fetchABook = async (text) => {
+    try {
+      const res = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${text}`
+      );
+      setResultFindABook(res.data.items);
+    } catch (error) {
+      console.error("Error fetching books", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Find a Book</h1>
+      <input
+        type="text"
+        value={findABook}
+        onChange={(event) => setfindABook(event.target.value)}
+      />
+      <ul>
+        {resultFindABook.length > 0 && (
+          <>
+            {resultFindABook.map((result, index) => (
+              <div key={index}>
+                <li>{result.volumeInfo.title}</li>
+              </div>
+            ))}
+          </>
+        )}
+      </ul>
+    </div>
+  );
+}
 export default App;
